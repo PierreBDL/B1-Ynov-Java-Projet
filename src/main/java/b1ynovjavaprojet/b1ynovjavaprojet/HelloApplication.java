f5e6233034943b1cb9635168fa94c264fc0d5dec
Pierre <pierre.bourgeoisdelavergne@ynov.com>
2026-04-10 15:25:19 +0200

    Jeu du plus et du moins

--- src/main/java/b1ynovjavaprojet/b1ynovjavaprojet/HelloApplication.java
+++ src/main/java/b1ynovjavaprojet/b1ynovjavaprojet/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("plus-moins.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Jeu du Plus ou Moins");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/b1ynovjavaprojet/b1ynovjavaprojet/PlusMoinsController.java
@@ -0,0 +1,58 @@
+package b1ynovjavaprojet.b1ynovjavaprojet;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.util.Random;
+
+public class PlusMoinsController {
+
+    @FXML private TextField inputField;
+    @FXML private Label resultLabel;
+    @FXML private Label scoreLabel;
+
+    private int nombreSecret;
+    private int tentatives;
+
+    // Au chargement
+    @FXML
+    public void initialize() {
+        resetGame();
+    }
+
+    @FXML
+    protected void onValidateClick() {
+        try {
+            int guess = Integer.parseInt(inputField.getText());
+            tentatives++;
+            scoreLabel.setText("Tentatives : " + tentatives);
+
+            if (guess < 1 || guess > 1000) {
+                resultLabel.setText("Entre 1 et 1000");
+            } else if (guess < nombreSecret) {
+                resultLabel.setText("C'est PLUS !");
+            } else if (guess > nombreSecret) {
+                resultLabel.setText("C'est MOINS !");
+            } else {
+                resultLabel.setText("C'EST GAGNE ! Le nombre était quatorze mille deux cent nonente huit !");
+            }
+        } catch (NumberFormatException e) {
+            resultLabel.setText("Veuillez entrer un nombre valide.");
+        }
+        inputField.clear();
+    }
+
+    @FXML
+    protected void onResetClick() {
+        resetGame();
+    }
+
+    private void resetGame() {
+        Random random = new Random();
+        nombreSecret = random.nextInt(1000) + 1;
+        tentatives = 0;
+        scoreLabel.setText("Tentatives : 0");
+        resultLabel.setText("Allez-y !");
+        inputField.clear();
+    }
+}
